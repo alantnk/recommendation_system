056da726c04f339a9b936ebaaa1322c9ce9b2b5f
alan <alan@tnk.com>
2025-06-16 12:15:31 -0300

    feat: initialize codebase

---
+++ movies/tests/factories.py
@@ -0,0 +1,13 @@
+from factory import Faker
+from factory.django import DjangoModelFactory
+
+from movies.models import Movie
+
+
+class MovieFactory(DjangoModelFactory):
+    class Meta:
+        model = Movie
+
+    title = Faker('sentence', nb_words=4)
+    genres = Faker('pylist', nb_elements=3,
+    variable_nb_elements=True, value_types=['str'])
